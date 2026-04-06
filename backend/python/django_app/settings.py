875424442a137c02c9ee3e30fe10f8c6b2d23f8c
Aakanksha Sharma <aakankshaasharma9@gmail.com>
2026-04-06 15:26:06 +0530

    Week 6: added product UI, filters and inventory overview

--- backend/python/django_app/settings.py
+++ backend/python/django_app/settings.py
@@ -63,9 +63,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'django_app.products',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware", 
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -73,6 +76,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 
 ROOT_URLCONF = "django_app.urls"
